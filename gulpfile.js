fb73f5d7c3a23824a0e3d4a306d63a65dff46e59
Yukikazu Tasaka <yukikazu-tasaka@ds-brand.jp>
2017-05-02 10:18:54 +0900

    引数を扱えるようにminimistを追加、zipを利用できるようにzip,unzipを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,40 @@
 const gulp = require('gulp');
+
+// minify関係
 const {phpMinify} = require('@cedx/gulp-php-minify');
 const uglify = require('gulp-uglify');
 const minifycss = require('gulp-minify-css');
 
-const inputDir = "input";
-const outputDir = "output";
+// 引数を扱えるように
+const minimist = require('minimist');
+
+// zip,unzip関係
+const zip = require('gulp-zip');
+const unzip = require('gulp-unzip');
+
+
+// 引数を配列に格納
+const argv = minimist(process.argv.slice(2));
+
+
+// 初期化
+var inputDir = (argv['i'])?argv['i']:'input';
+var outputDir = (argv['o'])?argv['o']:'output';
+
+var zipFile = (argv['z'])?argv['z']:'';
+var zipName = (argv['n'])?argv['n']:'archive.zip';
+
+
+gulp.task('unzip', () => gulp.src(zipFile)
+	.pipe(unzip())
+	.pipe(gulp.dest(inputDir))
+);
+
+// 指定ディレクトリを圧縮する
+gulp.task('zip', () => gulp.src(inputDir+"/**/*")
+	.pipe(zip(zipName))
+	.pipe(gulp.dest(outputDir))
+);
 
 gulp.task('minify:php', () => gulp.src(inputDir+'/**/*.php', {read: false})
 	.pipe(phpMinify({mode:'safe'}))
@@ -12,7 +42,8 @@ gulp.task('minify:php', () => gulp.src(inputDir+'/**/*.php', {read: false})
 );
 
 gulp.task('minify:js', () => gulp.src(inputDir+'/**/*.js')
-	.pipe(uglify({preserveComments: 'some'}))
+	// .pipe(uglify({preserveComments: 'some'}))
+	.pipe(uglify())
 	.pipe(gulp.dest(outputDir))
 );
 
